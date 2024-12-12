697017be5a79f10b44c487ebda2c8e6adcef7952
Adarshika Pandey <153723733+adarshika2005@users.noreply.github.com>
2024-12-12 18:08:43 +0530

    Add files via upload

---
+++ aq.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+  
+    int arr[]={1,2,3,4,5,6,7,8,9};
+   int n =sizeof(arr)/4;
+   for(int i=0;i<=n-1;i++){
+    cout<<arr[i]<<" ";
+
+   }
+  
+}
