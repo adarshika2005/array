f930760478654d98f407f33498ae921a3b1cd7c3
Adarshika Pandey <153723733+adarshika2005@users.noreply.github.com>
2024-12-15 11:46:37 +0530

    Add files via upload

---
+++ a1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    //declae
+    int num[18];
+    //accesing an array
+    cout<<"value at 0 index"<<num[0]<<endl;
+    //initialising an array
+    int second [3]=  {1,22, 4};
+    //accesing an element 
+    cout<<"value at 2 index "<<second[1]<<endl;
+        cout<<"every thingh is fine"<<endl;
+    return 0;
+}
